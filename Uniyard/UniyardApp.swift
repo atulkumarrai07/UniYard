d9be450ee82e948010e389773da61d67a8b1fdcd
Atul Kumar Rai <atulkumr@andrew.cmu.edu>
2021-10-13 22:40:11 -0400

    Initial Commit

---
+++ Uniyard/UniyardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniyardApp.swift
+//  Uniyard
+//
+//  Created by Atul Kumar Rai on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct UniyardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
