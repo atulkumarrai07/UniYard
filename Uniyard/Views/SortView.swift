3d9eb876dbe106567f2281cd77be4218d9ff6c59
Iris Hao <73666463+iriscatter@users.noreply.github.com>
2021-11-02 20:26:53 -0400

    Test-add Sort&Filter view files

---
+++ Uniyard/Views/SortView.swift
@@ -0,0 +1,20 @@
+//
+//  SortView.swift
+//  Uniyard
+//
+//  Created by Athena Mimi on 2021-11-02.
+//
+
+import SwiftUI
+
+struct SortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SortView_Previews: PreviewProvider {
+    static var previews: some View {
+        SortView()
+    }
+}
